94084e98e98249d254c07964fb87f76fb780d5a0
mogun123 <52413625+mogun123@users.noreply.github.com>
2026-08-09 20:19:27 +0530

    Update tabs.tsx

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -1,8 +1,6 @@
 "use client";
-
 import * as React from "react";
 import * as TabsPrimitive from "@radix-ui/react-tabs";
-
 import { cn } from "./utils";
 
 function Tabs({
@@ -26,7 +24,7 @@ function TabsList({
     <TabsPrimitive.List
       data-slot="tabs-list"
       className={cn(
-        "bg-muted text-muted-foreground inline-flex h-9 w-fit items-center justify-center rounded-xl p-[3px] flex",
+        "bg-muted text-muted-foreground inline-flex h-9 w-fit items-center justify-center rounded-xl p-[3px]",
         className,
       )}
       {...props}
